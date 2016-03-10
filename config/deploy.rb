1d472a5eac93bd31032190d2ceefcc2ac191747f
robot-den <w.nazmutdinov@gmail.com>
2016-03-10 16:21:56 +0500

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,55 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'deploy_me'
+set :repo_url, 'https://github.com/robot-den/deploy_me.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deploy/applications/deploy_me'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :info
+
+# Default value for :pty is false
+# set :pty, true
+
+# Копирующиеся файлы и директории (между деплоями)
+set :linked_files, %w{config/database.yml config/settings.yml .rbenv-vars}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Ruby свистелки
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_roles, :all
+
+# А это рекомендуют добавить для приложений, использующих ActiveRecord
+set :puma_init_active_record, true
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
